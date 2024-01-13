a14c1d03a266ba8e25c3c3124ff8532b03c98748
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-13 15:55:36 +0100

    Add function to calculate result of two numbers and operator

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ const divide = (a,b) => {
 let num1=0;
 let num2=0;
 let operator=''; 
+
+// Functions
+
+const operate = (operator,num1,num2) => {
+    switch (operator){
+        case '+' : return add(num1,num2);
+        case '-' : return subtract(num1,num2);
+        case '*' : return multiply(num1,num2);
+        case '/' : return divide(num1,num2);
+    }
+}
+
+console.log(operate('+',3,4));
