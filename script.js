dc2d00a7d96c2c306f076b918bae65deeea1048b
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-13 20:00:43 +0100

    Add function to populate display with numbers

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const divide = (a,b) => {
 
 // Variables
 
-let num1=0;
-let num2=0;
+let num1="";
+let num2="";
 let operator=''; 
 
 // Functions
@@ -32,4 +32,27 @@ const operate = (operator,num1,num2) => {
     }
 }
 
-console.log(operate('+',3,4));
+// Create the functions that populate the display when you click the number buttons. 
+// You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+// queryselctor für zahlen tasten
+// in array schreiben
+// array auf display ausgeben nach jedem klick
+
+// alternativ kann ich die zahlen auch einfach als ein String speichern und über + hinzufügen
+
+let numberOnDisplay="";
+
+const numberButtons = document.querySelectorAll('button.number');
+numberButtons.forEach( (element) => {
+    element.addEventListener('click', () => {
+        numberOnDisplay+=(element.id);
+        display(numberOnDisplay);
+        // console.log(numberOnDisplay);
+    })
+})
+
+const display = (numberOnDisplay) => {
+    let display = document.querySelector('#display');
+    display.textContent=numberOnDisplay;
+} 
