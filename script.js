afe917f534ac36886eebd3789fbe70e2916bb904
Falk B. <86295521+falk234@users.noreply.github.com>
2024-01-13 15:30:49 +0100

    Add basic math operators
    Add
    subtract
    Multiply
    Divide

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello");
+// add
+// subtract
+// multiply
+// divide
+
+const add = (a,b) => {
+    return a+b;
+}
+
+const subtract = (a,b) => {
+    return a-b;
+}
+
+const multiply = (a,b) => {
+    return a*b;
+}
+
+const divide = (a,b) => {
+    return a/b;
+}
+
+let a=2;
+let b=3;
+
+console.log(add(a,b)+"=> 5");
+console.log(subtract(a,b)+"=> -1");
+console.log(multiply(a,b)+"=> 6");
+console.log(divide(a,b)+" => 0,6");
